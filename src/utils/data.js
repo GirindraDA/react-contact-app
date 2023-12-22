d83f58a327d282b626056b006bd500328ed5cf08
giri <gdwiixa@gmail.com>
2023-12-22 13:24:24 +0700

    contact app first

---
+++ src/utils/data.js
@@ -0,0 +1,25 @@
+const getData = () => {
+    return [
+        {
+            id : 1,
+            name : 'Girindra Dwi',
+            tag: 'girin',
+            imageUrl: '/images/arifaizin.jpeg',
+        },
+        {
+            id : 2,
+            name : 'Rinda Dwi',
+            tag: 'rindada',
+            imageUrl: '/images/arifaizin.jpeg',
+        },
+        {
+            id : 3,
+            name : 'Antara',
+            tag: 'anta',
+            imageUrl: '/images/arifaizin.jpeg',
+        },   
+    ];
+}
+
+
+export { getData };
